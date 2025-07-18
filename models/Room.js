d2df190e25ce640604058adccf011e278e4dc0d2
Olanrewaju <lyrix7000@gmail.com>
2025-07-18 19:37:21 +0100

    updated

--- models/Room.js
+++ models/Room.js
@@ -4,11 +4,9 @@ const roomSchema = new mongoose.Schema({
   name: { type: String, required: true },
   isPrivate: { type: Boolean, default: false },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // New field for admins
+  admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   parentRoom: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
-  messages: [messageSchema],
   type: { type: String, enum: ['main', 'sub'], default: 'main' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Room', roomSchema);
-
