b39c135b7059b87aa7d5264b9ec8e510ba2ed9bf
Olanrewaju <lyrix7000@gmail.com>
2025-07-20 13:56:39 +0100

    updated no duplicate

--- sockets/chat.socket.js
+++ sockets/chat.socket.js
@@ -21,12 +21,16 @@ const chatSocket = (io) => {
     });
 
     // Send message to room
-    socket.on('room-message', async ({ sender, roomId, text }) => {
-      const message = await Message.create({ sender, room: roomId, text });
-      const populated = await message.populate('sender', 'username');
-      io.to(roomId).emit('receive-room', populated);
-
+    socket.on('room-message', ({ sender, roomId, text }) => {
+    // DO NOT save to DB here if already saved via REST
+    io.to(roomId).emit('receive-room', {
+      sender,
+      room: roomId,
+      text,
+      createdAt: new Date().toISOString(),
+      });
     });
+
 
     socket.on('disconnect', () => {
       console.log('🔴 User disconnected:', socket.id);
