263f237e44ec3902add20903d0f92ad7cdfcf9ed
Olanrewaju <lyrix7000@gmail.com>
2025-07-25 16:34:21 +0100

    updated message with avatar

--- models/Message.js
+++ models/Message.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  avatar: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // for 1-on-1
   room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },     // for groups
   text: String,
