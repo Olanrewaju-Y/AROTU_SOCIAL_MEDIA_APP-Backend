7b57a4fa1e0368d8e39ac0310542c2a75058970a
Olanrewaju <lyrix7000@gmail.com>
2025-08-01 08:51:23 +0100

    private

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -4,12 +4,14 @@ const {auth, admin} = require('../middleware/authMiddleware');
 const {
   getPrivateMessages,
   sendMessage,
+  createPrivateMessage,
   recentConversations
 } = require('../controllers/message.controller');
 
 router.post('/private', auth, sendMessage);
+router.post('/createPrivate', auth, createPrivateMessage);
 router.get('/recent-conversations', auth, recentConversations);
-
+// Get private messages between two users
 router.get('/private/:userId', auth, getPrivateMessages);
 
 
